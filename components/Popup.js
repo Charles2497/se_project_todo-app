92b09be6f6d3d894fafa41f81e7999bc5aaaf5e3
Charles2497 <109841876+Charles2497@users.noreply.github.com>
2025-07-28 19:22:42 -0400

    Update Popup.js

--- components/Popup.js
+++ components/Popup.js
@@ -4,22 +4,20 @@ class Popup {
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
   }
 
-  _handleEscapeClose(evt) {
+  _handleEscapeClose = (evt) => {
     if (evt.key === "Escape") {
       this.close();
     }
-  }
+  };
 
   open() {
     this._popupElement.classList.add("popup_visible");
-    document.addEventListener("keyup", (evt) => this._handleEscapeClose(evt));
+    document.addEventListener("keydown", this._handleEscapeClose);
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
-    document.removeEventListener("keyup", (evt) =>
-      this._handleEscapeClose(evt)
-    );
+    document.removeEventListener("keydown", this._handleEscapeClose);
   }
 
   setEventListeners() {
